79c24409fdc105331f35e08302376b8fcc5ead8c
wooyongjeong <wooyong.dev@gmail.com>
2024-03-05 02:24:40 +0900

    feat(BallNumber): Add equals & hashCode methods

--- src/main/java/baseball/BallNumber.java
+++ src/main/java/baseball/BallNumber.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Objects;
+
 public class BallNumber {
     public static final int MIN_NUM = 1;
     public static final int MAX_NUM = 9;
@@ -14,5 +16,22 @@ public class BallNumber {
         if (number < MIN_NUM || number > MAX_NUM) {
             throw new IllegalArgumentException("1~9의 숫자가 아닙니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BallNumber that = (BallNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
